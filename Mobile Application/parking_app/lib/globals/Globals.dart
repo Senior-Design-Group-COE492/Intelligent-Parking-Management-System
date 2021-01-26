b27687f5e44be0e74915f7426a7fd1ddd7b0f475
Ramy-Gendy <61162010+Ramy-Gendy@users.noreply.github.com>
2021-01-26 22:06:05 +0400

    Resubmit app

---
+++ Mobile Application/parking_app/lib/globals/Globals.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Globals {
+  static const String BROKER_IP_ADDRESS = 'broker.hivemq.com';
+  static const String IP_ADDRESS = '192.168.0.119';
+  static const String PORT = '3000';
+  static const AssetImage FORTRESS_ASSET_IMAGE =
+      AssetImage('assets/icons/code.png');
+}
