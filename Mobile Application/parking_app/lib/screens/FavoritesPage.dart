623cd222aecd227e803bb24cd74228de5a159279
Saif AlNajjar <sete39@users.noreply.github.com>
2021-02-17 22:59:56 +0400

    Addd AlertDialog when attempting to log out, moved position of log out button

--- Mobile Application/parking_app/lib/screens/FavoritesPage.dart
+++ Mobile Application/parking_app/lib/screens/FavoritesPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:parking_app/controller/LoginController.dart';
 import 'package:parking_app/handlers/LoginHandler.dart';
+import 'package:parking_app/screens/SettingsPage.dart';
 import 'package:parking_app/widgets/FavoritedParkingInfoWidget.dart';
 import 'package:parking_app/widgets/LoginSelectorWidget.dart';
 
@@ -10,16 +11,34 @@ class Favorites extends StatelessWidget {
   Widget build(BuildContext context) {
     final _orangeColor = context.theme.primaryColor;
     final _lightOrangeColor = Color(0xFFFFBFA0);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Favorites",
-            style: TextStyle(color: Colors.white, fontSize: 20)),
-      ),
-      // TODO: change the temperorary TextButton to the favorites widget
-      // also make signedIn state global?
-      body: GetBuilder<LoginController>(
-        init: LoginController(),
-        builder: (state) => state.isSignedIn
+
+    final settingsButton = IconButton(
+      icon: Icon(Icons.settings),
+      color: Colors.white,
+      onPressed: () => Get.to(SettingsPage()),
+      splashColor: Colors.black12,
+      highlightColor: Colors.black12,
+    );
+    final logOutButton = IconButton(
+      icon: Icon(Icons.logout),
+      color: Colors.white,
+      onPressed: () => _showAlertDialog(context),
+      splashColor: Colors.black12,
+      highlightColor: Colors.black12,
+    );
+
+    return GetBuilder<LoginController>(
+      init: LoginController(),
+      builder: (state) => Scaffold(
+        appBar: AppBar(
+          title: Text("Favorites",
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+          actions: [
+            state.isSignedIn ? logOutButton : Container(),
+            settingsButton
+          ],
+        ),
+        body: state.isSignedIn
             ? Center(
                 child: Column(
                   children: [
@@ -36,28 +55,37 @@ class Favorites extends StatelessWidget {
                       lng: 30,
                       predictions: [235],
                     ),
-                    FlatButton(
-                      child: Text(
-                        "Log Out",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                      color: _orangeColor,
-                      disabledColor: _lightOrangeColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      onPressed: () {
-                        LoginHandler.signOut();
-                      },
-                    )
                   ],
                 ),
               )
             : LoginSelectorWidget(),
       ),
+    );
+  }
+
+  Future<void> _showAlertDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Log Out'),
+          content: Text('Are you sure you want to log out of your account?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Get.back(),
+            ),
+            TextButton(
+              child: Text('Log Out'),
+              onPressed: () {
+                LoginHandler.signOut();
+                Get.back();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
