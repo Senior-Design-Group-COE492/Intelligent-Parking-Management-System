27b456222231929df1114d71b972e49e981b0be2
Mariam <g00074374@aus.edu>
2021-03-22 13:46:31 +0400

    Ability to push all parking info into 1 document
    I feel pain and suffering but there you go.
    The structure I followed is that you have and array of hashmaps. these hashmaps are hashmaps of another hashmaps such that you have the car park id as the key and the value is an array of the rest of the data as hashmaps. I feel pain. Im sorry

--- Backend_Server/app.py
+++ Backend_Server/app.py
@@ -7,22 +7,21 @@ from parking import Parking
 app = Flask(__name__)
 app.config.from_object("config")
 
-
 #initializing the sdk
 default_app = firebase_admin.initialize_app()
 
 ## not using this to auth because OAuth 2.0 tokens are not supported with  firestore
 #cred = credentials.Certificate("parkingapp-6ecfd-firebase-adminsdk-4dxe4-9280754b4c.json")
 #firebase_admin.initialize_app(cred)
-
-db = firestore.client()
+#db = firestore.client()
 
 @app.route("/")
 def main():
     print("server running")
-    #Parking.initialize_locations()   ##ONLY UNCOMMENT THIS WHEN YOU WANT TO RESET THE PARKING LOCATIONS COLLECTION
+    #result = Parking.initializeLocations()   ##ONLY UNCOMMENT THIS WHEN YOU WANT TO RESET THE PARKING LOCATIONS COLLECTION
+    result = True
     #start thread forr prediction
-    return jsonify(success = True)
+    return jsonify(success = result)
 
 ###Parking methods###
 
@@ -30,7 +29,5 @@ def main():
 @app.route("/parking",methods = ["GET"])
 def getParking():
     pass
+
 ######################
-
-
-
--- Backend_Server/parking.py
+++ Backend_Server/parking.py
@@ -1,25 +1,43 @@
 ### contains method parkings ###
 import pandas as pd
 import numpy as np
+from neural_network import NN
+import firebase_admin
+from firebase_admin import credentials,firestore
 
 class Parking:
 
-    def initialize_locations():
+    def initializeLocations():
         info_df = pd.read_csv('hdb-carpark-information-with-lat-lng.csv')
-        
+
+        db = firestore.client()
         #for every record in the df, store it in the parking_info collection using the parking ID as document ID
+        parking = []
         for i in range (0,len(info_df)):
-            data = {
-                u'address': str(info_df.iloc[i][2]),
-                u'car_park_type': str(info_df.iloc[i][3]),
-                u'type_of_parking_system': str(info_df.iloc[i][4]),
-                u'short_term_parking': str(info_df.iloc[i][5]),
-                u'free_parking': False if info_df.iloc[i][6] == 'NO' else True,
-                u'night_parking': True if info_df.iloc[i][7] == 'YES' else False,
-                u'car_park_decks': str(info_df.iloc[i][8]),
-                u'gantry_height': str(info_df.iloc[i][9]),
-                u'car_park_basement': True if info_df.iloc[i][10] == 'Y' else False,
-                u'location': (info_df.iloc[0][11] , info_df.iloc[i][12])
-            }
-            db.collection(u'parking_info').document(info_df.iloc[i][1]).set(data)
-        
+            parking.append({
+                str(info_df.iloc[i][1]):[ 
+                {u'address': str(info_df.iloc[i][2])},
+                {u'car_park_type': str(info_df.iloc[i][3])},
+                {u'type_of_parking_system': str(info_df.iloc[i][4])},
+                {u'short_term_parking': str(info_df.iloc[i][5])},
+                {u'free_parking': False if info_df.iloc[i][6] == 'NO' else True},
+                {u'night_parking': True if info_df.iloc[i][7] == 'YES' else False},
+                {u'car_park_decks': str(info_df.iloc[i][8])},
+                {u'gantry_height': str(info_df.iloc[i][9])},
+                {u'car_park_basement': True if info_df.iloc[i][10] == 'Y' else False},
+                {u'location': (info_df.iloc[0][11] , info_df.iloc[i][12])}]
+            })
+
+        data = {
+            u'parking' : parking
+        }
+        db.collection(u'parking_info').document('parkings').set(data)
+        return (True,)
+
+
+    def generatePrediction():
+        list_of_predictions = NN.generateEverything()
+        #loop through the predictions
+    
+    def getFilteredParkings():
+        pass
